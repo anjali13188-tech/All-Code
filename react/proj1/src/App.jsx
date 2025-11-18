e6c4d05fbd916fa98daf750b5dc0942af058dc29
Amit06112001 <amit06112001@gmail.com>
2025-11-18 23:37:42 +0530

    push react code

--- react/proj1/src/App.jsx
+++ react/proj1/src/App.jsx
@@ -1,12 +1,18 @@
 
 import './App.css'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Block from './pages/block'
 
 function App() {
   
 
   return (
     <>
-     <h1>hello react</h1>
+     <h1>anjali</h1>
+     <Home/>
+     <Contact/>
+     <Block/>
     </>
   )
 }
